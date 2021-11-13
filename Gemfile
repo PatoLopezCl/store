14a5d449653839de66894f461c4a4cbc49e185b7
PatoLopezCl <patolopez@gmail.com>
2021-11-13 13:23:34 -0300

    resolviendo saas

--- Gemfile
+++ Gemfile
@@ -53,3 +53,6 @@ gem 'faker'
     gem 'capistrano-rails', '~> 1.4'
     gem 'capistrano-passenger', '~> 0.2.0'
     gem 'capistrano-rbenv', '~> 2.1', '>= 2.1.4'
+
+gem 'ed25519'
+gem 'bcrypt_pbkdf'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '18.216.69.129/', user: 'ubuntu', roles: %w{app db web}
+server '18.216.69.129', user: 'ubuntu', roles: %w{app db web}
 
 
 
