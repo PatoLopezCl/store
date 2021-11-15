7c624bb86f97a0e77719de341927018cb7314600
PatoLopezCl <patolopez@gmail.com>
2021-11-15 20:39:50 -0300

    last dance 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many: order_details
-  has_many: orders, through: :order_details
+  has_many :order_details
+  has_many :orders, through: :order_details
 
   has_many :cart_products
   has_many :carts, through: :cart_products
