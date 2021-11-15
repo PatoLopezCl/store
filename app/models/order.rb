4afe04718b3829230c4c13e476d336c1f62049cb
PatoLopezCl <patolopez@gmail.com>
2021-11-15 20:18:41 -0300

    last dance

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,8 @@ class Order < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :status
 
-  has_many: order_details
-  has_many: products, through: :order_details
+  has_many :order_details
+  has_many :products, through: :order_details
 
   validates :number, presence: true, uniqueness: true
   validates :total, presence: true, numericality: true
